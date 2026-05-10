e7aa9b3faea6127c2815275f81f626cb888aec8c
Kaia-Token-Creator <kaiatokencreator@gmail.com>
2026-05-11 04:39:57 +0900

    Update chatadult.ts

--- functions/api/chatadult.ts
+++ functions/api/chatadult.ts
@@ -144,7 +144,7 @@ const personaName =
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "venice-uncensored-1-2",
+          model: "venice-uncensored",
           temperature: 0.6,
           top_p: 0.9,
           frequency_penalty: 0.8,
